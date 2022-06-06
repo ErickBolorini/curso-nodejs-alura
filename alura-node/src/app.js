58eaba4f1238d7b50ab6f5d4bed5300ed712623a
ErickBolorini <erick.bolorini@tjam.jus.br>
2022-06-06 13:53:15 -0400

    inicio curso de node aula 1

---
+++ alura-node/src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json())
+
+const livros = [
+    {id: 1, "título": "Senhor dos Anéis"},
+    {id: 2, "título": "O hobbit"}
+]
+
+app.get('/', (req,res) => {
+    res.status(200).send('Curso de node')
+})
+
+app.get('/livros', (req,res) => {
+    res.status(200).json(livros);
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('livro cadastrado com sucesso');
+})
+
+export default app;
