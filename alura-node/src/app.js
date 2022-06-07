f1f2e1c682df93f0da5f763a96229cece5cd612a
ErickBolorini <erick.bolorini@tjam.jus.br>
2022-06-07 13:58:07 -0400

    Aula 03 Node.js

--- alura-node/src/app.js
+++ alura-node/src/app.js
@@ -1,25 +1,56 @@
 import express from "express";
+import db from "./config/dbConnect.js";
+
+db.on("error", console.log.bind(console, "Erro de conexão"))
+
+db.once("open", () => {
+    console.log("Connexão com o banco feita com sucesso");
+})
 
 const app = express();
 
 app.use(express.json())
 
 const livros = [
-    {id: 1, "título": "Senhor dos Anéis"},
-    {id: 2, "título": "O hobbit"}
+  {id: 1, "titulo": "Senhor dos Aneis"},
+  {id: 2, "titulo": "O Hobiit"}
 ]
 
-app.get('/', (req,res) => {
-    res.status(200).send('Curso de node')
+app.get('/', (req, res) => {
+  res.status(200).send('Curso de Node');
+}) 
+
+app.get('/livros', (req, res) => {
+  res.status(200).json(livros)
 })
 
-app.get('/livros', (req,res) => {
-    res.status(200).json(livros);
+app.get('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id);
+  res.json(livros[index]);
 })
 
 app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('livro cadastrado com sucesso');
+  livros.push(req.body);
+  res.status(201).send('Livro foi cadastrado com sucesso')
 })
 
-export default app;
+app.put('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
+})
+
+app.delete('/livros/:id', (req, res) => {
+  let {id} = req.params;
+  let index = buscaLivro(id);
+  livros.splice(index, 1);
+  res.send(`Livro ${id} removido com sucesso`);
+})
+
+
+function buscaLivro(id) {
+  return livros.findIndex(livro => livro.id == id)
+}
+
+export default app
+
