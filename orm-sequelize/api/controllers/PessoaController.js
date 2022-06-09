172b13de88fa9e97b6f47ff32d20675065e8a216
ErickBolorini <erick.bolorini@tjam.jus.br>
2022-06-09 13:26:53 -0400

    continuando curso

--- orm-sequelize/api/controllers/PessoaController.js
+++ orm-sequelize/api/controllers/PessoaController.js
@@ -43,6 +43,16 @@ class PessoaController {
         }
     }
 
+    static async apagaPessoa(req, res) {
+        const { id } = req.params
+        try {
+            await database.Pessoas.destroy({ where: { id: Number(id) } })
+            return res.status(200).json({ mensagem: `id ${id} deletado`})
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
 
 
 }
