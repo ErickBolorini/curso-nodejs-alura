df58ce2dec3ced0de0db18e4083e3971d66687e8
ErickBolorini <erick.bolorini@tjam.jus.br>
2022-06-09 12:58:32 -0400

    continuando curso

--- orm-sequelize/api/routes/pessoasRoute.js
+++ orm-sequelize/api/routes/pessoasRoute.js
@@ -1,8 +1,12 @@
 const {Router} = require('express')
+const { route } = require('..')
 const PessoaController = require('../controllers/PessoaController')
 
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
+
 
 module.exports = router
