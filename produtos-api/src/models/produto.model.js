77a49b14db8494d5dbe0f1642ef062a5265e4fc6
ErickBolorini <erick.bolorini@tjam.jus.br>
2022-06-13 12:41:20 -0400

    aula2

---
+++ produtos-api/src/models/produto.model.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+exports.Produto = void 0;
+/* eslint-disable prettier/prettier */
+var Produto = /** @class */ (function () {
+    function Produto(codigo, nome, preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+    return Produto;
+}());
+exports.Produto = Produto;
